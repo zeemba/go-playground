3ad9c9bcc1b9c8dfe7eefe98fbc3dc2dd695da26
zeemba <abdulazeemanimasaun@gmail.com>
2019-06-26 17:35:31 +0000

    first class

---
+++ ninja1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x int
+var y string
+var z bool
+
+func main() {
+	x = 42
+	y = "James Bond"
+	z = true
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	s := fmt.Sprintf("%v\t%v\t%v", x, y, z)
+	fmt.Println(s)
+}
